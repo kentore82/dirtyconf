3448834c63031d8cb1a74751ba7745996b2a4179
Ken Tore Tallakstad <tallakstad@gmail.com>
2018-05-10 23:00:28 +0200

    Update ubuntu_add_partner_repo.sh

--- postinstall/ubuntu_add_partner_repo.sh
+++ postinstall/ubuntu_add_partner_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "deb http://archive.canonical.com/ubuntu xenial partner" >> /etc/apt/sources.list
 echo "deb-src http://archive.canonical.com/ubuntu xenial partner" >> /etc/apt/sources.list
-apt-get update
-apt-get upgrade -y
+apt-get update -y
+DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 apt-get install -y python-minimal
