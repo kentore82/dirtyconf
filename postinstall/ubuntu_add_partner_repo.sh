afa6ad43b14759b2b3b76be73dfd20a27e85555c
Ken Tore Tallakstad <tallakstad@gmail.com>
2018-05-10 23:16:34 +0200

    Update ubuntu_add_partner_repo.sh

--- postinstall/ubuntu_add_partner_repo.sh
+++ postinstall/ubuntu_add_partner_repo.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+update-locale LANGUAGE=en_US.UTF-8
+update-locale	LC_ALL=en_US.UTF-8
+
 echo "deb http://archive.canonical.com/ubuntu xenial partner" >> /etc/apt/sources.list
 echo "deb-src http://archive.canonical.com/ubuntu xenial partner" >> /etc/apt/sources.list
 apt-get update -y
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 apt-get install -y python-minimal
+apt install -y python-setuptools python-pip
