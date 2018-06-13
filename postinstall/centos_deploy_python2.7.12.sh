1eedba8f9822e17c1f03f41ed0bb3f63301a521b
Ken Tore Tallakstad <tallakstad@gmail.com>
2018-06-13 07:45:22 +0200

    Update centos_deploy_python2.7.12.sh

--- postinstall/centos_deploy_python2.7.12.sh
+++ postinstall/centos_deploy_python2.7.12.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#make sure route is present
-yum install -y net-tools
 
-#Add internet gateway
-route add default gw 10.180.37.172;
+# Add internet gateway
+ip route add default via 10.180.37.172;
 
+# Add yum epel
 yum install -y epel-release;
 yum -y update;
-yum install -y wget python-pip net-tools
 
+# Upgrade python2
+yum install -y wget python-pip net-tools
 mkdir /tmp/python2.7.12;
 cd /tmp/python2.7.12;
 wget https://big.sherpaconsulting.no/downloads/python-2.7.12-centos7-x86_64.tar.gz;
@@ -17,3 +17,11 @@ yum localinstall -y --nogpgcheck python-libs-2.7.12-8.el7.centos.x86_64.rpm pyth
 cd /;
 rm -rf /tmp/python2.7.12;
 
+# Add deploy user and copy ssh pub key from root
+adduser deployer;
+echo -e "changeme123\nchangeme123" | passwd deployer
+usermod -aG wheel username
+mkdir /home/deployer/.ssh;
+chown deployer:deployer /home/$USER/.ssh;
+cp /root/.ssh/authorized_keys /home/deployer/.ssh/authorized_keys
+chown deployer:deployer /home/deployer/.ssh/authorized_keys
